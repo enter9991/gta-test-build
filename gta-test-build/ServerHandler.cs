6162c9f7a413f4704bf6324182b299953cff728d
Kevin Bollinger <kevin.bollinger@hotmail.de>
2021-08-14 16:54:21 +0200

    .

---
+++ gta-test-build/ServerHandler.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using AltV.Net;
+
+namespace gta_test_build
+{
+    class ServerHandler : Resource
+    {
+        public override void OnStart()
+        {
+            Alt.Log("Console: Server wurde gestartet");
+        }
+
+        public override void OnStop()
+        {
+            Alt.Log("Console: Server wurde Gestopt");
+        }
+    }
+
+}
